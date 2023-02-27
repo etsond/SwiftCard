b36c15b07cba161f8ffd2117bf8cba6c41ef8810
Etson <etsond@ufl.edu>
2023-02-27 07:09:52 -0500

    adding images to dom

--- card-game/js/main.js
+++ card-game/js/main.js
@@ -1,23 +1,37 @@
 // card game war
 let deckId = ''
 
-document.querySelector("button").addEventListener("click", getDeck)
+
 
 // getting a new deck of id
-function getDeck(){
+function getDeck(){}
 fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        // storing the deck id in the variable
+        // storing the deckId from the request
         deckId = data.deck_id
+        
+
     })
     .catch(err => {
         console.log(`error ${err}`)
     })
 
-}
+
+
+document.querySelector("button").addEventListener("click", sparTwoDeck)
 
 function sparTwoDeck(){
     const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector("#player1").src = data.cards[0].image
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 }
