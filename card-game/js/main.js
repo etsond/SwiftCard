dc377e653ce1d6826b14dee78e9e279eebdbbb18
Etson <etsond@ufl.edu>
2023-02-27 08:38:40 -0500

    added a helper function to convert all the 52 cards to number

--- card-game/js/main.js
+++ card-game/js/main.js
@@ -30,19 +30,19 @@ function sparTwoDeck(){
         .then(data => {
             console.log(data)
             // player 1 card
-            document.querySelector("#player1").src = Number(data.cards[0].image)
+            document.querySelector("#player1").src = data.cards[0].image
             // player 2 cards
-            document.querySelector("#player2").src = Number(data.cards[1].image)
+            document.querySelector("#player2").src = data.cards[1].image
 
             // get the value from the api for the two 2 counts
-            let valueOfPlayerOne = data.cards[0].value
-            let valueOfPlayerTwo = data.cards[1].value
+            let valueOfPlayerOne = cardStringToNum(data.cards[0].value)
+            let valueOfPlayerTwo = cardStringToNum(data.cards[1].value)
 
             // checking for winner based on card value
             if(valueOfPlayerOne > valueOfPlayerTwo){
                 document.querySelector("h3").innerText = "Winner is Player1"
             }
-            else if(valueOfPlayerTwo > valueOfPlayerTwo){
+            else if(valueOfPlayerTwo > valueOfPlayerOne){
                 document.querySelector("h3").innerText = "Winner is Player 2"
             }else{
                 document.querySelector("h3").innerText = "Draw again"
@@ -51,4 +51,22 @@ function sparTwoDeck(){
         .catch(err => {
             console.log(`error ${err}`)
         })
+}
+
+function cardStringToNum(val){
+    if(val === "ACE"){
+        return 14
+
+    }else if(val === "KING"){
+        return 13
+
+    }else if(val === "QUEEN"){
+        return 12
+    }
+    else if(val === "JACK"){
+        return 11
+    }
+    else{
+        return val
+    }
 }
